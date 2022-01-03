352161e4037960b00d3cd954d607f44913732218
Elísio Mualumene <elisiogabrielmualumene@gmail.com>
2022-01-03 20:22:27 +0100

    home container

--- portfolio/src/Pages/Home.jsx
+++ portfolio/src/Pages/Home.jsx
@@ -1,17 +1,22 @@
+import {FaFacebook, FaLinkedin, FaTwitter} from 'react-icons/fa'
 import Style from './Style/Home.module.css'
+import avatar from './man.png'
+
 
 const Home = () => {
     return(
         <div className={Style.home}>
             <div className={Style.container}>
-                <img src="" alt="Avatar"/>
+                <img src={avatar} alt="avatar"/>
                 <h2>Elísio Mualumene</h2>
                 <p>I'm a Web Development and UI UX Designer</p>
-                <div className="social_media">
-                <img src="" alt="Facebook"/>
-                <img src="" alt="Linkedin"/>
-                <img src="" alt="Twitter"/>
-                </div>
+                <ul>
+                    <li><a href="https://facebook.com/elisiomualumene1" target="_blank" rel='noreferrer'><FaFacebook/></a></li>
+
+                    <li><a href="https://linkedin.com/in/elisiomualumene" target="_blank"  rel='noreferrer'><FaLinkedin/></a></li>
+
+                    <li><a href="https://twitter.com/elisiomualumene" target="_blank"  rel='noreferrer'><FaTwitter/></a></li>
+                </ul>
             </div>
         </div>
     );
